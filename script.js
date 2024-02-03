e16313d345030c88fbaefbb5ee0cc11804c82edc
devmahin <mahinhowlader123@gmail.com>
2024-02-03 21:10:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// const person = {
+//     name: 'John',
+//     age: 30,
+// };
+
+
+// let arry = ["mahin", "antor"];
+// function detailes(person) {
+//     if(typeof person === "object" && !Array.isArray(person)){
+//         console.log("valid object") 
+//     }else{
+//         // console.log("Not valid")
+//         return "Not valid"
+//     }
+//     return `You are ${person?.name}.his ${person?.age} years old.`
+// }
+
+// console.log(detailes(arry))
